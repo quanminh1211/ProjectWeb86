323710035c40e6b1500a17f4f7dcee0396ddbd1c
minh quan <trinhminhquan1211@gmail.com>
2024-12-25 23:49:43 +0700

    flashSale

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ import React from 'react';
 import Header from './container/header/header.jsx';
 import NavBar from './container/navBar/NavBar.jsx';
 import SliderImage from './container/slider/Slider.jsx';
+import FlashSale from './container/FlashSale/FlashSale.jsx';
 
 function App() {
 
   return (
     <>
       <Header />
+      <div className="content">
       <div className='contentTop'>
       <NavBar />
       <SliderImage />
+      </div>
+      <FlashSale />
       </div>
     </>
   )
